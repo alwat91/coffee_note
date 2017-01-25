499920133b5883f71409fd2fbfaefa61a4b71812
alan <alwat91@uga.edu>
2017-01-25 14:14:08 -0500

    Create new brew route working

--- controllers/brews.js
+++ controllers/brews.js
@@ -6,11 +6,32 @@ var authHelpers = require('../helpers/auth.js');
 
 // Brews index
 router.get('/', function(req, res){
-  // res.send(req.session.currentUser);
   User.findById( req.session.currentUser._id)
     .exec(function(err, user){
       if(err) {console.log(err);}
       res.send(user.brews);
+    });
+});
+
+// Brews create
+router.post('/new', function(req, res){
+  var brew = new Brew({
+    beanType: req.body.beanType,
+    brewMethod: req.body.brewMethod,
+    massBeans: req.body.massBeans,
+    grindSetting: req.body.grindSetting,
+    waterTemp: req.body.waterTemp,
+    brewTime: req.body.brewTime,
+    massWater: req.body.massWater,
+    rating: req.body.rating,
+    description: req.body.description
+  });
+  User.findById(req.session.currentUser._id)
+    .exec(function(err, user){
+      if(err) {console.log(err);}
+      user.brews.push(brew);
+      user.save();
+      res.send(user);
     });
 });
 
