5f33418f09d673612b5623456c0ea3e8d01e6e04
alan <alwat91@uga.edu>
2017-01-25 14:03:56 -0500

    Brew index route working

--- controllers/brews.js
+++ controllers/brews.js
@@ -1,11 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
+var Brew = require('../models/brew.js');
 var authHelpers = require('../helpers/auth.js');
 
 // Brews index
 router.get('/', function(req, res){
-  res.send('Brews controller hit!');
+  // res.send(req.session.currentUser);
+  User.findById( req.session.currentUser._id)
+    .exec(function(err, user){
+      if(err) {console.log(err);}
+      res.send(user.brews);
+    });
 });
 
 module.exports = router;
